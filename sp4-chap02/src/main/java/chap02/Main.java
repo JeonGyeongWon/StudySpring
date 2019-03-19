0175a590b52399938e481458c8b2d9475b866d90
전경원 <@DESKTOP-4OLQP43>
2019-03-19 17:00:07 +0900

    2장까지 완료

---
+++ sp4-chap02/src/main/java/chap02/Main.java
@@ -0,0 +1,15 @@
+package chap02;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		//xml파일 정보를 읽어와서 bean을 관리해주는 객체이다 *스프링 컨테이너라고도 한다.
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("스프링");
+		System.out.println(msg);
+		ctx.close();
+	}
+}
